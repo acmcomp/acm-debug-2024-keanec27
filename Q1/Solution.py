1b86f8cd106504ba48e843763ab0941390383ca0
Keane Coutinho <122078931+keanec27@users.noreply.github.com>
2024-12-05 13:30:02 +0400

    Update Solution.py

--- Q1/Solution.py
+++ Q1/Solution.py
@@ -1,17 +1,10 @@
-from flask import Flask, render_template, url_for, request, redirect
-
-app = Flask(__name__)
-
-
-class ACM_Debugging_Competition:
-    @app.route("/bye")
+from flask import Flask,render_template,url_for,request,redirect
+app=Flask(__name__)
+class ACM_Debugging_Competition():
+    @app.route('/')
     def index():
         return "hi"
-
-    @app.route("/hi")
-    def bye():
-        return "bye"
-
-
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=False)
+
+# Change the /hi to /
